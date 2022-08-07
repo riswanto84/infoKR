dfa80c504234c2e922bda6269d4117f6afab7d7d
riswanto84 <45375675+riswanto84@users.noreply.github.com>
2022-08-07 13:25:16 +0700

    pagination view

--- kendaraan/views.py
+++ kendaraan/views.py
@@ -2,10 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .decorators import check_superadmin, check_admin_and_superadmin
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Kendaraan
 
 @login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'kendaraan/dashboard.html')
+    kendaraan_list = Kendaraan.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(kendaraan_list, 10)
+    try:
+        kendaraan = paginator.page(page)
+    except PageNotAnInteger:
+        kendaraan = paginator.page(1)
+    except EmptyPage:
+        kendaraan = paginator.page(paginator.num_pages)
+
+    context = {
+        'kendaraan': kendaraan,
+    }
+    return render(request, 'kendaraan/dashboard.html', context)
 
 @login_required(login_url='login')
 @check_admin_and_superadmin
